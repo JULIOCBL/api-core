bf1c76a47724afda244833f00d6cc083010e9c10
JULIOCBL <60810419+JULIOCBL@users.noreply.github.com>
2026-02-08 12:30:20 -0500

    correcion de los comando

--- src/Core/Infrastructure/Console/Kernel.php
+++ src/Core/Infrastructure/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace Src\Core\Infrastructure\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Src\Core\Infrastructure\Console\Commands\GenerateKeyPair;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        GenerateKeyPair::class,
     ];
 
     /**
